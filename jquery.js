d38202b5c565a00cd82ebf326ae2231169099297
Thant Zaw <thantzawzinlin@gmail.com>
2020-01-28 12:44:02 +0630

    Animate Section

--- jquery.js
+++ jquery.js
@@ -1,3 +1,19 @@
 $(function () {
-    $('.greenbox').hide(1000);
+    // $('.greenbox').click(function () {
+    //     $(this).fadeTo(1000, 0).delay(1000).fadeTo(1000, 1)
+    // })
+    $('.redbox,.bluebox,.greenbox').click(function () {
+        $(this).animate({
+            marginLeft: '+200px',
+            borderRadius: '50%'
+        }, 1000).animate({
+            marginLeft: '-50px',
+
+        });
+        // $('.redbox,.bluebox,.greenbox').animate({
+        //     marginLeft: '-50px'
+
+
+        // }, 1000)
+    })
 });
